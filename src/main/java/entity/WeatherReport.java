14a410cee188277f66abaa016592c8ad424c6f99
Luca Piccone <lpiccone19@gmail.com>
2025-10-08 16:35:58 -0400

    Architecture

---
+++ src/main/java/entity/WeatherReport.java
@@ -0,0 +1,25 @@
+package entity;
+
+public class WeatherReport {
+    String latitude;
+    String longitude;
+    // cloudy, rainy, sunny...
+    String main;
+    String temp;
+    String feels_like;
+    String temp_min;
+    String temp_max;
+    String pressure;
+    String humidity;
+    String sea_level;
+    String ground_level;
+    String wind_speed;
+    String wind_deg;
+    String wind_gust;
+    String country;
+    String timezone;
+
+    public WeatherReport() {
+
+    }
+}
