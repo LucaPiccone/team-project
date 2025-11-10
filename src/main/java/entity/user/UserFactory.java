0e09cf62b2bf119c374bcc4f8c4296cf1624fba7
Denise Chang <ddleletan@hotmail.com>
2025-11-10 11:23:31 -0500

    implement UserFactory and LocationFactory. Placeholder in LocationFactory

--- src/main/java/entity/user/UserFactory.java
+++ src/main/java/entity/user/UserFactory.java
@@ -1,4 +1,10 @@
 package entity.user;
 
+import java.util.ArrayList;
+
 public class UserFactory {
+    public User create(String name, String password) {
+        // creates a new user with no saved locations
+        return new User(name, password, new ArrayList<>());
+    }
 }
