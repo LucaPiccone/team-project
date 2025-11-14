b2fcf1adfa9631410d075eff7a733c06cf13f170
Denise Chang <ddleletan@hotmail.com>
2025-11-14 16:49:08 -0500

    update locationfactory implementation of coordinates

--- src/main/java/entity/location/Location.java
+++ src/main/java/entity/location/Location.java
@@ -1,37 +1,35 @@
 package entity.location;
 
-import java.util.List;
-
 public class Location {
     private final String name;
-    private final double latitude;
     private final double longitude;
+    private final double latitude;
 
     /**
      * Creates a new location with the given non-empty name and non-empty password.
      * @param name the name of the city
-     * @param latitude the latitude
      * @param longitude the longitude
+     * @param latitude the latitude
      * @throws IllegalArgumentException if the name is empty
      */
 
-    public Location(String name, double latitude, double longitude) {
+    public Location(String name, double longitude, double latitude) {
         if ("".equals(name)) {
             throw new IllegalArgumentException("Username cannot be empty");
         }
         this.name = name;
-        this.latitude = latitude;
         this.longitude = longitude;
+        this.latitude = latitude;
     }
     public String getName() {
         return name;
     }
 
-    public double getLatitude() {
-        return latitude;
-    }
-
     public double getLongitude() {
         return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
     }
 }
--- src/main/java/entity/location/LocationFactory.java
+++ src/main/java/entity/location/LocationFactory.java
@@ -1,10 +1,26 @@
 package entity.location;
 
+import api.geocodingapi.*;
+
+import java.util.HashMap;
+
 public class LocationFactory {
-    public Location create(String name) {
-        // placeholder => need to update using geocoding API to convert name to lat/long
-        double latitude = 2.0;
-        double longitude = 3.0;
-        return new Location(name, latitude, longitude);
+
+    private final CoordinatesFetcher fetcher;
+
+    public LocationFactory(CoordinatesFetcher fetcher) {
+        this.fetcher = fetcher;
+    }
+
+    /**
+     * Create a Location object given a city name.
+     */
+    public Location create(String name) throws CoordinatesFetcher.CityNotFoundException {
+        HashMap<String, Double> coordinates = fetcher.getCoordinates(name);
+
+        double longitude = coordinates.get("lon");
+        double latitude = coordinates.get("lat");
+
+        return new Location(name, longitude, latitude);
     }
 }
