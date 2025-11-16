fb0c99a8e20ffba73b38bce420c15704a88a70e7
dell <@gmai.com>
2025-11-15 21:12:25 -0500

    fix #19

---
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -0,0 +1,20 @@
+package interface_adapter.logout;
+
+/**
+ * The controller for the Logout Use Case.
+ */
+public class LogoutController {
+
+    private LogoutInputBoundary logoutUseCaseInteractor;
+
+    public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Logout Use Case.
+     */
+    public void execute() {
+        logoutUseCaseInteractor.execute();
+    }
+}
