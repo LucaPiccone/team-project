064137677ee7eaf03ec55f68d67260cde37b642d
Luca Piccone <lpiccone19@gmail.com>
2025-11-22 03:15:10 -0500

    Added auto complete suggestions to the loggedInSearchView.

--- src/main/java/use_case/loggedInSearchPage/LoggedInSearchPageInteractor.java
+++ src/main/java/use_case/loggedInSearchPage/LoggedInSearchPageInteractor.java
@@ -1,17 +1,30 @@
 package use_case.loggedInSearchPage;
 
+import api.googlePlacesAPI.GooglePlacesFetcher;
+import api.googlePlacesAPI.PlaceFetcher;
+import entity.placeSuggestions.PlaceSuggestion;
 import use_case.loggedInHomePage.LoggedInHomePageInputBoundary;
 import use_case.loggedInHomePage.LoggedInHomePageOutputBoundary;
 
+import java.util.List;
+
 public class LoggedInSearchPageInteractor implements LoggedInSearchPageInputBoundary {
     private final LoggedInSearchPageOutputBoundary userPresenter;
+    private final GooglePlacesFetcher fetcher;
 
-    public LoggedInSearchPageInteractor(LoggedInSearchPageOutputBoundary loggedInSearchPageOutputBoundary) {
+    public LoggedInSearchPageInteractor(LoggedInSearchPageOutputBoundary loggedInSearchPageOutputBoundary, GooglePlacesFetcher fetcher) {
         userPresenter = loggedInSearchPageOutputBoundary;
+        this.fetcher = fetcher;
     }
 
     @Override
     public void switchToLoggedInHomePageView() {
         userPresenter.switchToLoggedInHomePageView();
+    }
+
+    @Override
+    public void fetchSuggestions(String query) throws PlaceFetcher.PlaceNotFoundException {
+        List<PlaceSuggestion> suggestions = fetcher.getPlace(query);
+        userPresenter.showSuggestionsToUser(suggestions);
     }
 }
